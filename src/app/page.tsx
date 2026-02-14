d03485fd3299de3acd265eec8699ef5c964a6a02
aidengoldkr <aidengoldkr@gmail.com>
2026-02-15 02:06:19 +0900

    dlrjtwjrjt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <section className={`${styles.about} ${styles.scrollArea}`} id="navBright">
           <div className={styles.aboutContent}>
             <div className={styles.aboutTitle}>
-              <img className={styles.aboutLogo} src="./icon.svg"></img>
+              <img className={styles.aboutLogo} src="/home-icon.svg"></img>
               <div className={styles.aboutText}>
                 <p className={styles.contentTitle}>ESSENTIA Science</p>
                 <p className={styles.contentDesc}>ESSENTIA 과학회는 시험 성적으로는 드러나지 않는 과학적 열정과 잠재력을 가진 학생들을 위한 비공인 과학회입니다. 학교에서 경험하기 어려운 깊이 있는 탐구와 교류의 기회를 통해, 진정한 과학 인재로 성장할 수 있는 환경을 만드는 것을 목표로 합니다</p>
